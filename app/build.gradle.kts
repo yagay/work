b60f640683626c43de32846bdaac54238d4c5a3d
Jie <1614094+yagay@users.noreply.github.com>
2026-08-24 20:23:45 +0100

    Add Android app Gradle config

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.example.workhours"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.workhours"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 3
+        versionName = "1.2"
+    }
+}
