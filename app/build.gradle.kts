70b52af34308e042f442c2428f668a784a04e8a5
Jie <1614094+yagay@users.noreply.github.com>
2026-08-24 21:14:08 +0100

    Bump version to 1.15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.example.workhours"
         minSdk = 31
         targetSdk = 37
-        versionCode = 14
-        versionName = "1.14"
+        versionCode = 15
+        versionName = "1.15"
     }
 }
